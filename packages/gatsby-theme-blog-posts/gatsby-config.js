373a7b607d67b57b176cf7ac19ee43ce52506dfe
Wojciech Malarski <w.malarski@gmail.com>
2021-04-25 22:25:33 +0200

    basic contentful config added

--- packages/gatsby-theme-blog-posts/gatsby-config.js
+++ packages/gatsby-theme-blog-posts/gatsby-config.js
@@ -1,9 +1,25 @@
+// import { config } from "dotenv";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+  debug: true
+});
+
 module.exports = {
   plugins: [
     {
       resolve: "gatsby-plugin-page-creator",
       options: {
         path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     }
   ]
